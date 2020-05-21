a41c17142d54c1a24e2a411370c2395be6a15d98
Tech Tutorials <jatindera@gmail.com>
2020-05-21 07:06:53 +0530

    intial code

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,6 @@
+var template = /*#__PURE__*/React.createElement("p", {
+    id: "myId"
+  }, "This is JSX from app.js");
+
+var appRoot = document.getElementById("app")
+ReactDOM.render(template, appRoot)
